32bc5784e3e9bf4b44ec54d1e885731164907419
Anh Nhật <nhathuynh04work@gmail.com>
2025-12-25 13:33:36 +0700

    add validation for minimum length and presence to password field

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@ class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
 
-    # authentication
+    # password
     has_secure_password
+    
+    # has_secure_password only check for empty password not blank password so we need to add presence: true
+    validates :password, length: { minimum: 6 }, presence: true 
 end
