77f6b3693702cdf5635871d29927162dc3e8e9f6
Anh Nhật <nhathuynh04work@gmail.com>
2025-12-25 11:14:37 +0700

    index the email column of users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_25_022603) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_25_040553) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,5 +19,6 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_25_022603) do
     t.string "email"
     t.string "name"
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
