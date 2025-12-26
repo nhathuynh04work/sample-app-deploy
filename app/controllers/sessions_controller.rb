ca406fb0f4d302f293b6108920574673da6711cc
Anh Nhật <nhathuynh04work@gmail.com>
2025-12-26 16:42:46 +0700

    add handler for POST /login and DELETE /logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
-  def new
-  end
+    def new
+    end
+
+    def create
+        user = User.find_by(email: params[:session][:email].downcase)
+
+        if user && user.authenticate(params[:session][:password])
+            # Log the user in and redirect to profile page
+        else
+            flash[:danger] = "Invalid email/password"
+            render "new", status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+
+    end
 end
