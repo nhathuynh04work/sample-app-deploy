4c237f4f8fb2c97f44057e17ae7fd66ea0314cd2
Anh Nhật <nhathuynh04work@gmail.com>
2025-12-26 15:48:49 +0700

    create session controller as a RESTful endpoint for login/logout and add new routes to the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+    get "/login", to: "sessions#new"
+    post "/login", to: "session#create"
+    delete "/logout", to: "session#destroy"
+
     get "/signup", to: "users#new"
+
     get "/help", to: "static_pages#help"
     get "/about", to: "static_pages#about"
     get "/contact", to: "static_pages#contact"
