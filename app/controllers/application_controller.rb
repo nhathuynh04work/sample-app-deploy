c9dda011e778eb3d7b79d9525febe7d3ef7ccf82
Anh Nhật <nhathuynh04work@gmail.com>
2025-12-26 21:10:55 +0700

    create a log_in helper and make it avaiable to all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+    # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+    allow_browser versions: :modern
 
-  # Changes to the importmap will invalidate the etag for HTML responses
-  stale_when_importmap_changes
+    # Changes to the importmap will invalidate the etag for HTML responses
+    stale_when_importmap_changes
+
+    # Make SessionsHelper available to all controllers
+    include SessionsHelper
 end
