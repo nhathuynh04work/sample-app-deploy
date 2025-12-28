6be74d954217e389547e9817cbc4a3508e5d1a87
Anh Nhật <nhathuynh04work@gmail.com>
2025-12-28 22:36:59 +0700

    add remember_digest field to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_25_042643) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_28_153546) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_25_042643) do
     t.string "email"
     t.string "name"
     t.string "password_digest"
+    t.string "remember_digest"
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
