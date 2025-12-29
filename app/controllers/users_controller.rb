f79172281b1095053245c13eada33a40a0aaeb3b
Anh Nhật <nhathuynh04work@gmail.com>
2025-12-29 16:21:41 +0700

    check if the user making the request is the same as the target before editing and updating

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     before_action :logged_in_user, only: [:edit, :update]
+    before_action :correct_user, only: [:edit, :update]
 
     def new
         @user = User.new
@@ -22,12 +23,10 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
+        # @user is retrieved by correct_user
     end
 
     def update
-        @user = User.find(params[:id])
-
         if @user.update(user_params)
             flash[:success] = "Profile updated"
             redirect_to @user
@@ -46,5 +45,11 @@ class UsersController < ApplicationController
                 flash[:danger] = "Please log in."
                 redirect_to login_url
             end
+        end
+
+        def correct_user
+            # This also sets the @user in the action it runs before
+            @user = User.find(params[:id])
+            redirect_to root_url unless @user == current_user
         end
 end
