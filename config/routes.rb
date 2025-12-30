4757aa22b290b114f0f00153fb5653c665a49aa2
Anh Nhật <nhathuynh04work@gmail.com>
2025-12-30 11:12:19 +0700

    create AccountAcitivations controller and create an edit route for it

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,22 @@
 Rails.application.routes.draw do
+    # auth
     get "/login", to: "sessions#new"
     post "/login", to: "sessions#create"
     delete "/logout", to: "sessions#destroy"
 
     get "/signup", to: "users#new"
 
+    # static
     get "/help", to: "static_pages#help"
     get "/about", to: "static_pages#about"
     get "/contact", to: "static_pages#contact"
 
+    # users
     resources :users
 
+    # account activation
+    resources :account_activations, only: [:edit]
+
+    # root
     root "static_pages#home"
 end
