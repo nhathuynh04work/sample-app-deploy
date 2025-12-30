19ee8f4ebe6a97c0e3cc1cb7c0b09361943c15bb
Anh Nhật <nhathuynh04work@gmail.com>
2025-12-30 08:35:09 +0700

    add index controller action to UsersController to display a list of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-    before_action :logged_in_user, only: [:edit, :update]
+    before_action :logged_in_user, only: [:edit, :update, :index]
     before_action :correct_user, only: [:edit, :update]
+
+    def index
+        @users = User.all
+    end
 
     def new
         @user = User.new
@@ -35,6 +39,7 @@ class UsersController < ApplicationController
         end
     end
 
+    # Private
     private
     def user_params
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
