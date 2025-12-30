96536c87a0fd450894ec6b177d454ad41e91cdce
Anh Nhật <nhathuynh04work@gmail.com>
2025-12-30 10:03:10 +0700

    integration test for pagination

--- config/initializers/will_paginate_renderer.rb
+++ config/initializers/will_paginate_renderer.rb
@@ -6,28 +6,28 @@ module WillPaginate
             protected
 
             def html_container(html)
-                tag(:ul, html, container_attributes)
+                tag(:div, html, container_attributes)
             end
 
             def page_number(page)
                 if page == current_page
-                    tag(:li, link(page, page, class: "page-link"), class: "page-item active")
+                    tag(:div, link(page, page, class: "page-link"), class: "page-item active")
                 else
-                    tag(:li, link(page, page, rel: rel_value(page), class: "page-link"), class: "page-item")
+                    tag(:div, link(page, page, rel: rel_value(page), class: "page-link"), class: "page-item")
                 end
             end
 
             def gap
-                tag(:li, link("&hellip;", "#", class: "page-link"), class: "page-item disabled")
+                tag(:div, link("&hellip;", "#", class: "page-link"), class: "page-item disabled")
             end
 
 
             def previous_or_next_page(page, text, classname, aria_label = nil)
                 if page
-                    tag(:li, link(text, page, class: "page-link"), class: "page-item #{classname}")
+                    tag(:div, link(text, page, class: "page-link"), class: "page-item #{classname}")
                 else
-                    tag(:li, link(text, "#", class: "page-link"), class: "page-item disabled #{classname}")
-                end
+                    tag(:div, link(text, "#", class: "page-link"), class: "page-item disabled #{classname}")
+                end 
             end
         end
     end
