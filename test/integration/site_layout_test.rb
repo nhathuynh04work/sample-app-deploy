f593e8d7e231def200b4756dd2f2fb84862f2ce5
Anh Nhật <nhathuynh04work@gmail.com>
2025-12-30 08:44:21 +0700

    write integration test for site layout

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+    def setup 
+        @user = users(:michael)
+    end
+
     test "layout links" do
         get root_path
         assert_template "static_pages/home"
@@ -8,8 +12,17 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
         assert_select "a[href=?]", help_path
         assert_select "a[href=?]", about_path
         assert_select "a[href=?]", contact_path
+        assert_select "a[href=?]", login_path
 
-        get contact_path
-        assert_select "title", full_title("Contact")
+        log_in_as(@user)
+        follow_redirect!
+        assert_template "users/show"
+        assert_select "a[href=?]", root_path, count: 2
+        assert_select "a[href=?]", help_path
+        assert_select "a[href=?]", about_path
+        assert_select "a[href=?]", contact_path
+        assert_select "a[href=?]", users_path
+        assert_select "a[href=?]", user_path
+        assert_select "a[href=?]", edit_user_path
     end
 end
