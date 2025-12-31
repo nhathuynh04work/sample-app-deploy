08cfbcdbb728fa5f61d9379828eb69a009df849c
Anh Nhật <nhathuynh04work@gmail.com>
2025-12-31 14:56:02 +0700

    add password reset related field to the User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_30_042421) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_31_075211) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -24,6 +24,8 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_30_042421) do
     t.string "name"
     t.string "password_digest"
     t.string "remember_digest"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
