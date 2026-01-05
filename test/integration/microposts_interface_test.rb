934e36c50549147beb89d63f6ec6f6bcfc8c3232
Anh Nhật <nhathuynh04work@gmail.com>
2026-01-05 15:43:09 +0700

    write test case for sidebar

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -32,9 +32,24 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
         assert_difference 'Micropost.count', -1 do
             delete micropost_path(first_micropost)
         end
-        
+
         # Visit different user (no delete links)
         get user_path(users(:archer))
         assert_select 'a', text: 'delete', count: 0
+    end
+
+    test "micropost sidebar count" do
+        log_in_as(@user)
+        get root_path
+        assert_match "#{@user.microposts.count} microposts", response.body
+
+        # User with zero microposts
+        other_user = users(:malory)
+        log_in_as(other_user)
+        get root_path
+        assert_match "0 microposts", response.body
+        other_user.microposts.create!(content: "A micropost")
+        get root_path
+        assert_match "1 micropost", response.body
     end
 end
