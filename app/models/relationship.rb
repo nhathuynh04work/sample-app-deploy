98271dc69ec57f282cc3c8eab036dc9a132307b5
Anh Nhật <nhathuynh04work@gmail.com>
2026-01-06 10:20:03 +0700

    write test and validation for Relationship

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,8 @@
 class Relationship < ApplicationRecord
     belongs_to :follower, class_name: "User"
     belongs_to :followed, class_name: "User"
+
+    # the following 2 lines are optional but actually needed in older Rails versions
+    validates :follower_id, presence: true
+    validates :followed_id, presence: true
 end
